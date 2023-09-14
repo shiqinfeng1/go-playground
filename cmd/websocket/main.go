6c73d8803f78e34a75add2406e98a9071cf7a89c
shiqinfeng <shiqinfeng@jmail.com>
2023-09-15 07:51:34 +0800

    feat: test mutli websocket

--- cmd/websocket/main.go
+++ cmd/websocket/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"log"
 
@@ -14,25 +15,38 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+type wsConn struct {
+	c *websocket.Conn
+}
+
+var connPool = make(map[string]wsConn)
+
 func main() {
-	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/eventSub", func(w http.ResponseWriter, r *http.Request) {
 		log.Println("")
 		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
-
 		for {
 			// Read message from browser
-			msgType, msg, err := conn.ReadMessage()
+			_, msg, err := conn.ReadMessage()
 			if err != nil {
 				return
 			}
-
-			// Print the message to the console
-			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
-
-			// Write message back to browser
-			if err = conn.WriteMessage(msgType, msg); err != nil {
-				return
+			if strings.HasPrefix(string(msg), "subID") {
+				connPool[string(msg)] = wsConn{
+					c: conn,
+				}
+				break
 			}
+		}
+	})
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn := connPool[r.FormValue("id")]
+		// Print the message to the console
+		fmt.Printf("%s sent: %s\n", conn.c.RemoteAddr(), r.FormValue("id"))
+
+		// Write message back to browser
+		if err := conn.c.WriteMessage(websocket.TextMessage, []byte(r.FormValue("id"))); err != nil {
+			return
 		}
 	})
 
